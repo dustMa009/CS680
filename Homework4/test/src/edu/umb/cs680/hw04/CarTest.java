bfe68e046a9c5fb029e1c681db1bdc46bfdecd19
dustMa009 <61129250+dustMa009@users.noreply.github.com>
2021-10-21 13:59:55 -0400

    Add files via upload

---
+++ Homework4/test/src/edu/umb/cs680/hw04/CarTest.java
@@ -0,0 +1,20 @@
+package edu.umb.cs680.hw04;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    private String eo1 = System.getProperty("line.separator");
+
+    private String[] CarToStringArray(Car c) {
+        String[] res = {c.getMake(), c.getModel(), String.valueOf(c.getYear())};
+        return res;
+    }
+
+    @Test
+    public void verifyCarEqualityWithMakeModelYear() {
+        Car firstCar = new Car("abc", "tesla", 9000, 2010, 1500f);
+        Car secondCar = new Car("abc", "tesla", 7000, 2010, 2000f);
+        assertArrayEquals(CarToStringArray(firstCar), CarToStringArray(secondCar));
+    }
+}
