3db8fa65dd06233ecb55b6626f383e1f24a63217
dustMa009 <61129250+dustMa009@users.noreply.github.com>
2021-12-26 00:32:55 -0500

    Delete Homework5 directory

--- Homework5/src/edu/umb/cs680/hw05/State.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.hw05;
-
-public interface State {
-    public void openCloseButtonPushed(DVDPlayer player);
-    public void playButtonPushed(DVDPlayer player);
-    public void stopButtonPushed(DVDPlayer player);
-}
